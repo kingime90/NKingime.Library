0d9339dfcb3baa58c0342ef8b8a01728ab47553e
kingime90 <kingime90@163.com>
2018-01-17 22:35:59 +0800

    创建NKingime.Library

---
+++ NKingime.Core/Extentsion/GeneralExtentsion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NKingime.Core.Extentsion
+{
+    /// <summary>
+    /// 常规扩展。
+    /// </summary>
+    public static class GeneralExtentsion
+    {
+
+        /// <summary>
+        /// 指示指定的泛型类型是否为 null。
+        /// </summary>
+        /// <typeparam name="T">泛型类型。</typeparam>
+        /// <param name="t">泛型实例。</param>
+        /// <returns>如果 t 参数为 null，则为 true；否则为 false。</returns>
+        public static bool IsNull<T>(this T t)
+        {
+            return t == null;
+        }
+
+        /// <summary>
+        /// 指示指定的泛型类型是否不为 null。
+        /// </summary>
+        /// <typeparam name="T">泛型类型。</typeparam>
+        /// <param name="t">泛型实例。</param>
+        /// <returns>如果 t 参数不为 null，则为 true；否则为 false。</returns>
+        public static bool IsNotNull<T>(this T t)
+        {
+            return !IsNull(t);
+        }
+
+        /// <summary>
+        /// 获取实例或默认实例。
+        /// </summary>
+        /// <typeparam name="T">泛型类型。</typeparam>
+        /// <param name="t">泛型实例。</param>
+        /// <param name="defVal">默认泛型实例。</param>
+        /// <returns>如果 t 参数不为 null，则为 t；如果 defVal 参数不为 null，则为 defVal；否则为泛型类型的default（默认值）。</returns>
+        public static T GetOrDefault<T>(this T t, T defVal)
+        {
+            if (t.IsNotNull()) return t;
+            //
+            if (defVal.IsNotNull()) return defVal;
+            //
+            return default(T);
+        }
+    }
+}
---
+++ NKingime.Core/Extentsion/StringExtentsion.cs
@@ -0,0 +1,88 @@
+﻿using NKingime.Core.Flag;
+
+namespace NKingime.Core.Extentsion
+{
+    /// <summary>
+    /// 字符串扩展方法。
+    /// </summary>
+    public static class StringExtentsion
+    {
+
+        /// <summary>
+        /// 指示指定的字符串是 null 还是 System.String.Empty 字符串。
+        /// </summary>
+        /// <param name="value">要测试的字符串。</param>
+        /// <returns>如果 value 参数为 null 或空字符串 ("")，则为 true；否则为 false。</returns>
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        /// <summary>
+        /// 指示指定的字符串是 null、空还是仅由空白字符组成。
+        /// </summary>
+        /// <param name="value">要测试的字符串。</param>
+        /// <returns>如果 value 参数为 null 或 System.String.Empty，或者如果 value 仅由空白字符组成，则为 true。</returns>
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="defVal"></param>
+        /// <returns></returns>
+        public static string GetString(this string value, string defVal = "")
+        {
+            return value.GetOrDefault(defVal);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="trimFlag"></param>
+        /// <param name="trimChars"></param>
+        /// <returns></returns>
+        public static string GetString(this string value, TrimStringFlag trimFlag, params char[] trimChars)
+        {
+            return GetString(value, string.Empty, trimFlag, trimChars);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="defVal"></param>
+        /// <param name="trimFlag"></param>
+        /// <param name="trimChars"></param>
+        /// <returns></returns>
+        public static string GetString(this string value, string defVal, TrimStringFlag trimFlag, params char[] trimChars)
+        {
+            return value.GetOrDefault(defVal).Trim(trimFlag, trimChars);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="trimFlag"></param>
+        /// <param name="trimChars"></param>
+        /// <returns></returns>
+        public static string Trim(this string value, TrimStringFlag trimFlag, params char[] trimChars)
+        {
+            switch (trimFlag)
+            {
+                case TrimStringFlag.Start:
+                    return value.TrimStart(trimChars);
+                case TrimStringFlag.End:
+                    return value.TrimEnd(trimChars);
+                case TrimStringFlag.All:
+                    return value.Trim(trimChars);
+            }
+            return value;
+        }
+    }
+}
