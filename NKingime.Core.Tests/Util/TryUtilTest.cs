77ac8a19b2e2049a53bc2d31088c10b5652400ec
aiming.wang <aiming.wang@fmchexian.com>
2018-01-19 15:58:04 +0800

    日志记录器

---
+++ NKingime.Core.Tests/Util/TryUtilTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using NKingime.Core.Util;
+using NKingime.Core.General;
+
+namespace NKingime.Core.Tests.Util
+{
+    /// <summary>
+    /// 尝试应用单元测试。
+    /// </summary>
+    [TestFixture]
+    public class TryUtilTest
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void Action()
+        {
+
+        }
+    }
+}
---
+++ NKingime.Core/Log/ILogger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NKingime.Core.Log
+{
+    /// <summary>
+    /// 日志记录器。
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// 是否启用。
+        /// </summary>
+        bool Enabled { get; }
+
+        /// <summary>
+        /// 优先级（倒序）。
+        /// </summary>
+        int Priority { get; }
+
+        /// <summary>
+        /// 记录调试。
+        /// </summary>
+        /// <param name="message">消息。</param>
+        void Debug(string message);
+
+        /// <summary>
+        /// 记录调试。
+        /// </summary>
+        /// <param name="message">消息。</param>
+        /// <param name="exception">异常。</param>
+        void Debug(string message, Exception exception);
+
+        /// <summary>
+        /// 记录错误。
+        /// </summary>
+        /// <param name="message">消息。</param>
+        void Error(string message);
+
+        /// <summary>
+        /// 记录错误。
+        /// </summary>
+        /// <param name="message">消息。</param>
+        /// <param name="exception">异常。</param>
+        void Error(string message, Exception exception);
+
+        /// <summary>
+        /// 记录信息。
+        /// </summary>
+        /// <param name="message">消息。</param>
+        void Info(string message);
+
+        /// <summary>
+        /// 记录信息。
+        /// </summary>
+        /// <param name="message">消息。</param>
+        /// <param name="exception">异常。</param>
+        void Info(string message, Exception exception);
+    }
+}
